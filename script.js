118cfdeaa36fe5e168be2274f9e9a9ab33894c22
Juana Saavedra <juanavsaavedrar@gmail.com>
2025-05-12 08:21:47 -0500

    Initial version of webpage

---
+++ script.js
@@ -0,0 +1,6 @@
+// Placeholder for future interactivity or animations
+document.querySelectorAll('.graphic-placeholder').forEach(function (el) {
+    el.addEventListener('click', function () {
+        alert('Imagen correspondiente será cargada aquí');
+    });
+});
